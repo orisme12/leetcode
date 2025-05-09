6ec8caa05d77c0ad5914a8035b285f18ba05b8f4
orisme12 <carlosbonet33@gmail.com>
2025-05-08 21:45:45 -0500

    Aprendizaje de lenguajes

---
+++ JavaScript/main.js
@@ -0,0 +1,9 @@
+
+function main() {
+    
+    // Declaracion de Variables 
+    let name = "Carlos"
+    const PI = 3.1416;
+    
+
+}
---
+++ TypeScript/main.ts
@@ -0,0 +1,5 @@
+function mainITypeScript() {
+  // Declaracion de Variables
+  let name = "Carlos";
+  const PI = 3.1416;
+}
